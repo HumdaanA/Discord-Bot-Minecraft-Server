9741962d93a5461d1982a7d07b4a1e634c57900c
HumdaanA <157570972+HumdaanA@users.noreply.github.com>
2025-02-05 21:15:05 -0600

    Update start_stop_instance.py

--- start_stop_instance.py
+++ start_stop_instance.py
@@ -1,3 +1,11 @@
+# This here is the PYTHON code for the Lambda function
+# Here, it waits for an action from API Gateway, whether it is a stop or start action,
+# and starts/stops the MINECRAFT server instance accordingly
+
+# In order to use it, however, when creating the lambda resource in terraform, you must
+# compress the file and state the file name in the resource block
+# so, this file was zipped into the filed named "lambda_function.zip"
+
 
 import boto3
 import json
@@ -5,7 +13,7 @@ import json
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    instance_id = '[MY INSTANCE-ID FOR MINECRAFT SERVER INSTANCE]'  // i left out the ID of the minecraft server instance
+    instance_id = '[MY INSTANCE-ID FOR MINECRAFT SERVER INSTANCE]'  # i left out the ID of the minecraft server instance
     action = event['queryStringParameters']['action']
 
     if action == 'start':
